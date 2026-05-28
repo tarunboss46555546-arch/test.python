f7efd836e641b8ac39ba1020a2b46718974c90be
tarunboss46555546-arch <tarunboss46555546@gmail.com>
2026-05-28 13:07:58 +0530

    Add files via upload

---
+++ test .py
@@ -0,0 +1,21 @@
+int(input random)
+secrect_number = random.randint(1, 50)
+attempt = 5
+won = False
+print("while attempts > 0 and not won:")
+print("Enter a valid number")
+if guess == secrect_number
+print("You got it! You won!")
+won = True
+else:
+   attempts -= 1
+if <=5:
+   print("Hint: You're hot!")
+elif <= 15:
+   print("Hint: Your warm")
+elif <= 30
+   print("Hint: You;re cold")
+for i in range(attempts):
+   print(end=" ")
+if not won:
+   print(Game over)
